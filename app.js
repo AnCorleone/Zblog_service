9bd366fc8490a81a4bd55c53cf56ea817c0b7a68
张安然 <anakin@newsupplytech.com>
2022-09-07 23:35:55 +0800

    fix 获取不到res.cc ,需将自定义统一处理中间件放在token解析中间件之前

--- app.js
+++ app.js
@@ -9,16 +9,6 @@ const { secretKey} =  require('./src/utils/config')
 // const { expressjwt: jwt } = require("express-jwt");
 
 const app = express()
-// 配置解析 Token 的中间件
-app.use(jwt({ secret: secretKey, algorithms: ["HS256"] }).unless({ path: ['/api/user/register','/api/user/login'] }))
-// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
-// app.use(jwt({ secret: secretKey, algorithms: ["HS256"] }).unless({ path: [/^\/api\//] }))
-// 配置跨域中间件
-app.use(cors())
-app.use(express.json())
-//配置表单解析中间件
-app.use(express.urlencoded({ extended: true }))
-
 // 自定义全局中间件
 app.use((req,res,next)=>{
   res.cc= function (err,status =1,data) {
@@ -32,7 +22,15 @@ app.use((req,res,next)=>{
   }
   next()
 })
-
+// 使用 .unless({ path: [/^\/api\//] }) 指定哪些接口不需要进行 Token 的身份认证
+// app.use(jwt({ secret: secretKey, algorithms: ["HS256"] }).unless({ path: [/^\/api\//] }))
+// 配置跨域中间件
+app.use(cors())
+app.use(express.json())
+//配置表单解析中间件
+app.use(express.urlencoded({ extended: true }))
+// 配置解析 Token 的中间件
+app.use(jwt({ secret: secretKey, algorithms: ["HS256"] }).unless({ path: ['/api/user/register','/api/user/login'] }))
 // 配置路由中间件
 app.use(userRouter)
 
@@ -45,13 +43,14 @@ app.get('/', (req, res) => {
 })
 // 错误处理
 app.use((err, req, res,next) => {
-  //  此处res.cc 不止为何为undefined
-  // if (err.name == 'UnauthorizedError') return res.cc('身份认证失败!')
-  if (err.name === 'UnauthorizedError') return res.json({
-    status :1,
-    message : '身份认证失败!'
-  })
-  res.send(err)
+  //TODO  此处res.cc 不止为何为undefined
+  // fix 获取不到res.cc ,需将自定义统一处理中间件放在token解析中间件之前
+  if (err.name == 'UnauthorizedError') return res.cc('身份认证失败!')
+  // if (err.name === 'UnauthorizedError') return res.json({
+  //   status :1,
+  //   message : '身份认证失败!'
+  // })
+  res.cc(err)
 })
 
 app.listen('3007', () => {
